e0f8497c2078f19012f9ae9b433158d8ee9784f8
AaronTorresM <63605067+AaronTorresM@users.noreply.github.com>
2020-04-13 15:45:07 -0500

    timeline
    Se actualizo el timeline

--- js/tabla.js
+++ js/tabla.js
@@ -108,3 +108,13 @@ $(document).ready(function () {
 
 
 });
+
+
+// funcion timeline
+
+	var completes = document.querySelectorAll(".complete");
+
+	function toggleComplete(){
+	  var lastComplete = completes[completes.length - 1];
+	  lastComplete.classList.toggle('complete');
+	}
