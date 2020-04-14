ae1d6c35f576f0fc2411465272edfc7a305c46b0
Polo <raibel_19@hotmail.com>
2020-04-14 00:56:51 -0500

    estilos y filtros
    se cambian estilos , se agrega funcionalidad de filtro, y se agregan librerias nuevas

--- js/tabla.js
+++ js/tabla.js
@@ -48,100 +48,117 @@ var data3 = [
 
 var inpescIngreso = [
   {
-    DocComercial: 90071871,
-    OrdenCompra: 10023818,
+    DocComercial: 9040471872,
+    OrdenCompra: 1054023838,
+    monto: "$24,681.00",
+    creado: "01/04/2020",
+    estatus: "Verificar SAC",
+    estatusId: 3,
+    destino:
+      "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
+  },
+  {
+    DocComercial: 9008971874,
+    OrdenCompra: 10345023858,
+    monto: "$20,681.00",
+    creado: "11/02/2020",
+    estatus: "Concluido",
+    estatusId: 2,
+    destino:
+      "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
+  },
+  {
+    DocComercial: 95471871,
+    OrdenCompra: 100345818,
     monto: "$29,681.00",
     creado: "11/04/2020",
-    estatus: "Verificar SAC",
+    estatus: "Pendiente",
+    estatusId: 1,
     destino:
       "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
   },
   {
-    DocComercial: 90071872,
-    OrdenCompra: 10023838,
-    monto: "$24,681.00",
-    creado: "11/04/2020",
-    estatus: "Verificar SAC",
-    destino:
-      "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
-  },
-  {
-    DocComercial: 90071873,
-    OrdenCompra: 10023858,
-    monto: "$21,681.00",
-    creado: "11/04/2020",
-    estatus: "Verificar SAC",
-    destino:
-      "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
-  },
-  {
-    DocComercial: 90071874,
-    OrdenCompra: 10023858,
-    monto: "$20,681.00",
-    creado: "11/04/2020",
-    estatus: "Verificar SAC",
-    destino:
-      "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
-  },
-  {
-    DocComercial: 90071875,
-    OrdenCompra: 10023898,
+    DocComercial: 93450071875,
+    OrdenCompra: 1008923898,
     monto: "$22,681.00",
-    creado: "11/04/2020",
-    estatus: "Verificar SAC",
+    creado: "11/02/2020",
+    estatus: "Pendiente",
+    estatusId: 1,
     destino:
       "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
   },
   {
-    DocComercial: 90071876,
-    OrdenCompra: 10023828,
+    DocComercial: 90056671873,
+    OrdenCompra: 1007823858,
+    monto: "$21,681.00",
+    creado: "11/03/2020",
+    estatus: "Verificar SAC",
+    estatusId: 3,
+    destino:
+      "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
+  },
+  {
+    DocComercial: 9003571876,
+    OrdenCompra: 1008923828,
     monto: "$25,681.00",
-    creado: "11/04/2020",
-    estatus: "Verificar SAC",
+    creado: "11/01/2020",
+    estatus: "Concluido",
+    estatusId: 2,
     destino:
       "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
   },
   {
-    DocComercial: 90071871,
-    OrdenCompra: 10023818,
+    DocComercial: 9007201871,
+    OrdenCompra: 100239818,
     monto: "$27,681.00",
     creado: "11/04/2020",
     estatus: "Verificar SAC",
+    estatusId: 3,
     destino:
       "Con. Suc. Periférico / Tablaje Castral 97210 Merida Sin distrito / colonia",
   },
 ];
 
 $(document).ready(function () {
-  $("#example").DataTable({
-    paging: true,
-    searching: false,
-    lengthChange: false,
-    data: inpescIngreso,
-    scrollY: true,
+  var $expampleDT = null;
+  var expampleDTFunc = function (data) {
+    $expampleDT = $("#example").DataTable({
+      paging: true,
+      searching: false,
+      lengthChange: false,
+      data: data,
+      scrollY: true,
 
-    columns: [
-      { data: "DocComercial", class: "colorLetra" },
-      { data: "OrdenCompra", class: "colorLetra" },
-      { data: "monto", class: "colorLetra" },
-      { data: "creado", class: "colorLetra" },
-      { data: "estatus", class: "colorLetra" },
-      { data: "destino", class: "colorLetra" },
-    ],
-    columnDefs: [
-      {
-        targets: -1,
-        className: "dt-body-right",
+      columns: [
+        { data: "DocComercial", class: "colorLetra", orderable: false },
+        { data: "OrdenCompra", class: "colorLetra", orderable: false },
+        { data: "monto", class: "colorLetra" },
+        { data: "creado", class: "colorLetra" },
+        { data: "estatus", class: "colorLetra" },
+        { data: "destino", class: "colorLetra", width: '35%' },
+      ],
+      columnDefs: [
+        {
+          targets: -1,
+          className: "dt-body-right",
+        },
+      ],
+      fnRowCallback: function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+        if (iDisplayIndex % 2 == 0) {
+          $("td", nRow).css("background-color", "#00afd7");
+        } else {
+          $("td", nRow).css("background-color", "#ffff");
+        }
       },
-    ],
-    fnRowCallback: function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-      if (iDisplayIndex % 2 == 0) {
-        $("td", nRow).css("background-color", "#00afd7");
-      } else {
-        $("td", nRow).css("background-color", "#ffff");
+      initComplete: function (settings, json) {
+        setTimeout(function () {
+          $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+        }, 200);
       }
-    },
-  });
+    });
+  }
+
+  expampleDTFunc(inpescIngreso);
 
   $("#example2").DataTable({
     paging: true,
@@ -210,26 +227,120 @@ $(document).ready(function () {
     },
   });
 
-  $(window)
-    .off()
-    .on("resize", function () {
+  var events = function () {
+    $(window).off().on("resize", function () {
       setTimeout(function () {
         $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
       }, 100);
     });
 
-  $("tbody > tr .colorLetra").on("click", function () {
-    $("#myModal").modal("toggle");
+    $("tbody > tr .colorLetra").off().on("click", function () {
+      $("#myModal").modal("toggle");
+    });
+
+    // Select all tabs
+    $(".nav-tabs a").off().on('click', function () {
+      debugger;
+      $(this).tab("show");
+      setTimeout(function () {
+        $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+      }, 200);
+    });
+
+    $("#setFilters").off().on('click', function (e) {
+      var estatus = $("#select_estatus :selected").val(),
+        fechaInicio = $("#date_start").val(),
+        fechaFin = $("#date_end").val(),
+        por = $("#select_por :selected").val();
+
+      var filter = inpescIngreso.filter(function (a, b) {
+        var convFecha = moment(a.creado, 'DD/MM/YYYY');
+        return (!fechaInicio || convFecha >= moment(fechaInicio, 'DD/MM/YYYY')) &&
+          (!fechaFin || convFecha <= moment(fechaFin, 'DD/MM/YYYY')) &&
+          (!estatus || estatus == 0 || a.estatusId == estatus)
+      });
+
+      if (por) {
+        switch (por) {
+          case '1':
+            filter.sort(function (a, e) {
+              return e.DocComercial - a.DocComercial;
+            });
+            break;
+          case '2':
+            filter.sort(function (a, e) {
+              return e.OrdenCompra - a.OrdenCompra;
+            });
+            break;
+          default:
+            break;
+        }
+      }
+
+      if ($expampleDT) {
+        $expampleDT.clear().destroy();
+      }
+
+      expampleDTFunc(filter);
+      events();
+    });
+  };
+
+  events();
+
+  function Select2Languaje() {
+    return {
+      noResults: function () { return "No hay resultados" },
+      searching: function () { return "Buscando..." },
+      errorLoading: function () { return "No se pudieron cargar los resultados" },
+      inputTooShort: function (e) {
+        t = e.minimum - e.input.length, n = "Por favor, introduzca " + t + " car"; return t == 1 ? n += "ácter" : n += "acteres", n
+      }
+    };
+  }
+
+  $('.dropdownActive').select2({
+    theme: "bootstrap",
+    allowClear: true,
+    placeholder: "Seleccione una opción",
+    language: Select2Languaje(),
+    multiple: false,
+    width: "100%",
   });
 
-  // Select all tabs
-  $(".nav-tabs a").click(function () {
-    debugger;
-    $(this).tab("show");
-    setTimeout(function () {
-      $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
-    }, 200);
+  $("div.date.date-start")
+    .datepicker({
+      format: "dd/mm/yyyy",
+      weekStart: 0,
+      maxViewMode: 2,
+      todayBtn: "linked",
+      language: "es",
+      orientation: "bottom auto",
+      daysOfWeekHighlighted: "0,6",
+      autoclose: true,
+      todayHighlight: true,
+      startDate: moment("01/01/1900", "DD/MM/YYYY").format("DD/MM/YYYY"),
+      endDate: moment(new Date(), "DD/MM/YYYY").format("DD/MM/YYYY"),
+    }).off().on("changeDate", function (e) {
+      var selectDate = moment(e.date).format("DD/MM/YYYY");
+      $("div.date.date-end").datepicker("setStartDate", selectDate);
+      $("div.date.date-end").datepicker("setDate", selectDate);
+    });
+
+  $("div.date.date-end").datepicker({
+    format: "dd/mm/yyyy",
+    weekStart: 0,
+    maxViewMode: 2,
+    todayBtn: "linked",
+    language: "es",
+    orientation: "bottom auto",
+    daysOfWeekHighlighted: "0,6",
+    autoclose: true,
+    todayHighlight: true,
+    startDate: "01/01/1900",
   });
+
+
 });
 
 // funcion timeline
