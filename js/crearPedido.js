f17c8669af0faca375339c9a11f2709d5e76854d
Jose Angel Flores <52080294+AngelFFlores@users.noreply.github.com>
2020-04-29 22:38:09 -0500

    l&f

--- js/crearPedido.js
+++ js/crearPedido.js
@@ -2,18 +2,17 @@ $(document).ready(function() {
     $('.isResizable').matchHeight();
 
 
-    $('#headingTwo').prop('disabled',true);
+
     $('#headingThree').prop('disabled',true);
   
 
     $("#btnContinuarColp1").on("click", function(){
-        $('#headingTwo').prop('disabled',false);
-        $('#headingTwo').click();
-    });
-
-    $("#btnContinuarColp2").on("click", function(){
         $('#headingThree').prop('disabled',false);
         $('#headingThree').click();
         $('.isResizable').matchHeight();
+    });
+
+    $("#btnContinuarColp2").on("click", function(){
+      
     });
 });
